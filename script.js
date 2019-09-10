1ca495d7194c1a4862c597b73f2872e7b0bd9988
Alen <Alenslo@gmx.net>
2019-09-10 21:31:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let user = {
+    username: "Alen",
+    password: "open",
+};
+
+let database = [user];
+
+let newsfeed = [
+    {
+        username: "Rookie", 
+        timeline: "I am a beginner"
+    }, 
+    {
+        username: "Sofi", 
+        timeline: "My journey never ends",
+    }, 
+    {
+        username: "Naoki", 
+        timeline: "Japan is beautiful"
+    },
+];
+
+
+document.write(database[0].username);
+document.write(newsfeed[2].username);
