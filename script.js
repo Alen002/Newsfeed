b827ff527bf7c71795817c65284a6804ae6868c5
Alen <Alenslo@gmx.net>
2019-09-11 11:29:27 +0200

    User authentification implemented

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-let user = {
-    username: "Alen",
-    password: "open",
-};
-
-let database = [user];
+let database = [
+    {
+        username: 'Sofi',
+        password: 'open'
+    },
+    {
+        username: 'Naoki',
+        password: 'japan'
+    },
+];
 
 let newsfeed = [
     {
-        username: "Rookie", 
-        timeline: "I am a beginner"
-    }, 
-    {
-        username: "Sofi", 
+        username: "Alex", 
         timeline: "My journey never ends",
     }, 
     {
-        username: "Naoki", 
+        username: "Bob", 
         timeline: "Japan is beautiful"
     },
 ];
 
+//let username = prompt('Please enter your name');
+//let password = prompt('Please enter your password');
 
-document.write(database[0].username);
-document.write(newsfeed[2].username);
+//function logIn(username, password) {
+
+//}
+
+function validate() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    
+
+    database.forEach((user) => {
+        if(username == user.username && password == user.password) {
+            alert('Successful');
+            window.location = 'welcome.html'; 
+            return false;
+        } 
+    })
+   
+
+}
